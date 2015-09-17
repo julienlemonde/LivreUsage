4d910c8a322ca71997d0c6737204f18857c6012d
unknown <am36790@logti-a3324-01.ens.ad.etsmtl.ca>
2015-09-17 11:17:25 -0400

    debut

---
+++ WebApplication2/WebApplication2/Models/IdentityModels.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace WebApplication2.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public string Telephone { get; set; }
+        public string Courriel { get; set; }
+        
+
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+    }
+}
